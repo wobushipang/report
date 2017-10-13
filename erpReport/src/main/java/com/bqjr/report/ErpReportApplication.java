d23989ee202c0d2f563272dd1ca7c88138fe7708
18218725800@163.com <18218725800@163.com>
2017-10-13 10:05:46 +0800

    report

---
+++ erpReport/src/main/java/com/bqjr/report/ErpReportApplication.java
@@ -0,0 +1,12 @@
+package com.bqjr.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpReportApplication.class, args);
+	}
+}
