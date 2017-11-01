9bcd2383a4c2f06b3b86428eba185666fea58e57
weihuang <weihuangw@isoftstone.com>
2017-11-01 17:56:14 +0800

    测试测试测试

---
+++ erpReport/src/main/java/com/bqjr/report/controller/SaleTotal.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Copyright (C) 2010-2020 Billions Finance. All Rights Reserved.
+ * ============================================================================
+ * 版权所有 2010-2020 佰仟金融服务有限公司，并保留所有权利
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！您不能在任何未经允许的前提下对程序代码进行修改和使用；不允许
+ * 对程序代码以任何形式任何目的的再发布
+ * ============================================================================
+ *
+ * @ProjectName erp
+ * @PackageName com.bqjr.report.controller
+ * @FileName SaleTotal 
+ * @author wei.huang02
+ * @Date 2017年11月1日 下午5:43:08 
+ *****************************************************************************/
+package com.bqjr.report.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName SaleTotal.java
+ * @Description 
+ * @author wei.huang02
+ * @Date 2017年11月1日 下午5:43:08
+ * @since JDK 1.8
+ */
+@Controller
+public class SaleTotal {
+	
+}
